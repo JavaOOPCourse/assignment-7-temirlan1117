c0d82c0847b39e97292e52212790b441a8e82446
temirlan1117 <temirlanormushev@yandex.ru>
2026-03-25 22:19:23 +0600

    HM7 Ormushov Temirlan

--- src/StudentRecordProcessor.java
+++ src/StudentRecordProcessor.java
@@ -1,4 +1,5 @@
-
+import java.io.*;
+import java.util.*;
 public class StudentRecordProcessor {
     // Поля для хранения данных
     private final List<Student> students = new ArrayList<>();
@@ -14,22 +15,99 @@ public class StudentRecordProcessor {
      */
     public void readFile() {
         // TODO: реализуйте чтение файла здесь
+        try (BufferedReader br = new BufferedReader(new FileReader("input/students.txt"))){
+            String line;
+            while ((line = br.readLine()) != null){
+                try{
+                    String [] parts = line.split(",");
+                    String name = parts[0];
+                    int score = Integer.parseInt(parts[1]);
+                    if (score < 0 || score > 100) {
+                        throw new InvalidScoreException("Invalid score");
+                    }
+                    students.add(new Student(name, score));
+                    System.out.println(line);
+
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("Invalid data: " + line);
+
+                } catch (InvalidScoreException e) {
+                    System.out.println("Invalid data: " + line);
+
+                } catch (ArrayIndexOutOfBoundsException e) {
+                    System.out.println("Invalid data: " + line);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found");
+
+        } catch (IOException e) {
+            System.out.println("I/O error");
+        }
     }
+            
 
     /**
      * Task 3 + Task 8
      */
     public void processData() {
         // TODO: обработка данных и сортировка здесь
+        if (students.size() == 0) {
+            averageScore = 0;
+            highestStudent = null;
+            return;
+        }
+
+        int sum = 0; // сумма всех баллов
+        highestStudent = students.get(0);
+        for (Student s : students) {
+            sum += s.getScore();
+            if (s.getScore() > highestStudent.getScore()) {
+                highestStudent = s;
+            }
+        }
+
+
+        averageScore = (double) sum / students.size();
+        Collections.sort(students, new Comparator<Student>() {
+            @Override
+            public int compare(Student a, Student b) {
+                return b.getScore() - a.getScore();
+            }
+        });
     }
+
+    
 
     /**
      * Task 4 + Task 5 + Task 8
      */
     public void writeFile() {
         // TODO: запись результата в файл здесь
-    }
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("output/report.txt"))){
+            bw.write("avarage" + averageScore);
+            bw.newLine();
+            if (highestStudent != null){
+                bw.write("highest" + highestStudent.getName() + highestStudent.getScore());}
+                else{
+                    bw.write("no high");
+                }
+            bw.newLine();
+            for (Student s : students){
+                bw.write(s.getName() + s.getScore());
+                bw.newLine();
 
+            }
+        }catch (IOException e) {
+            // если ошибка записи в файл
+            System.out.println("I/O error");
+        }
+
+            
+        }
+    
     public static void main(String[] args) {
         StudentRecordProcessor processor = new StudentRecordProcessor();
 
@@ -45,4 +123,27 @@ public class StudentRecordProcessor {
 }
 
 // class InvalidScoreException реализуйте меня
+class InvalidScoreException extends Exception {
+    public InvalidScoreException(String message) {
+        super(message);
+    }
+}
 // class Student (name, score)
+class Student {
+    private String name;
+    private int score;
+
+    public Student(String name, int score) {
+        this.name = name;
+        this.score = score;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore() {
+        return score;
+    }
+}
